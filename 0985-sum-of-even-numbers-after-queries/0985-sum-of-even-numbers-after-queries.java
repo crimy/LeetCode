3c9f6d106b9b1e7bbdeb52d38c8bcdf88430659f
서승원 <97273627+crimy@users.noreply.github.com>
2023-04-13 21:22:31 +0900

    Time: 5 ms (76.95%), Space: 50.4 MB (57.80%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.java
@@ -2,13 +2,17 @@ class Solution {
     public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int length = queries.length;
         int[] answer = new int[length];
+        int result = 0;
+        for(int num : nums) {
+            result += calculate(num);
+        }
         for(int index=0; index<length; index++) {
             int targetIndex = queries[index][1];
             int value = queries[index][0];
+            result -= calculate(nums[targetIndex]);
             nums[targetIndex] +=value;
-            for(int newNum : nums) {
-                answer[index] += calculate(newNum);
-            }
+            result += calculate(nums[targetIndex]);
+            answer[index] = result;
         }
         return answer;
     }
